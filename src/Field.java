291adb9a3ac0fa9bff7f8d0c5b276152eba79996
kpasc <koby.pascual@gmail.com>
2016-03-31 09:51:08 -0500

    Added goal boundary to field

--- src/Field.java
+++ src/Field.java
@@ -9,11 +9,11 @@ import com.jogamp.opengl.GLAutoDrawable;
  * @author kobypascual
  *
  */
-public class Field extends Shape{
+public class Field extends Shape {
 
-	protected double percentHeight = 1;
-	protected double percentWidth = 1;
-	protected double radius = 300;
+	protected double percentHeight = .95;
+	protected double percentWidth = .95;
+	protected double radius = (percentHeight*percentWidth)/.00275;
 
 	/**
 	 *
@@ -34,7 +34,8 @@ public class Field extends Shape{
 		drawField(gl,255,255,255);
 		drawLines(gl);
 		drawCircles(gl);
-		drawLinesInCircles(gl);
+		drawGoalBoundary(gl);
+		//loadImageCenterIce(gl);
 	}
 
 	private void drawField(GL2 gl, double red, double green, double blue){
@@ -54,19 +55,17 @@ public class Field extends Shape{
 
 	private void drawLines(GL2 gl)
 	{
-		// TODO deal with line sizes
+		// TODO deal with line sizes between Operating systems
 		// Draw the lines
-		gl.glLineWidth(50f);
+		gl.glLineWidth(20);
 		gl.glBegin(GL2.GL_LINES);
 			// Center line
 			setColor(gl,255,0,0);
 			gl.glVertex2d(convertWidth(view.getWidth()*0), convertHeight(view.getHeight()*percentHeight));
 			gl.glVertex2d(convertWidth(view.getWidth()*0), convertHeight(view.getHeight()*-percentHeight));
 
-			// TODO some math issues here, I'm wanting to divide this into thirds essentially
-			// TODO it's late and i'm not getting the math right.
 			// Left and right lines
-			setColor(gl,0,0,255);
+			setColor(gl,68,214,243);
 			gl.glVertex2d(convertWidth(view.getWidth()*-(1.0/3)*percentWidth), convertHeight(view.getHeight()*percentHeight));
 			gl.glVertex2d(convertWidth(view.getWidth()*-(1.0/3)*percentWidth), convertHeight(view.getHeight()*-percentHeight));
 			gl.glVertex2d(convertWidth(view.getWidth()*(1.0/3)*percentWidth), convertHeight(view.getHeight()*percentHeight));
@@ -76,10 +75,8 @@ public class Field extends Shape{
 	}
 
 	private void drawCircles(GL2 gl){
-		// TODO circle size is currently hard coded. need to do it as a percentage with the window size
-
-		double x = convertWidth(view.getWidth()*(1.0/1.75));
-		double y = convertHeight(view.getHeight()*(1.0/2));
+		double x = convertWidth(view.getWidth()*(1.0/1.75)*percentWidth);
+		double y = convertHeight(view.getHeight()*(1.0/2)*percentHeight);
 
 		drawCircle(gl,radius,0,0,255,0,0);		// Center circle
 		drawCircle(gl,radius*.7,-x,y,255,0,0);		// Left top circle
@@ -95,6 +92,7 @@ public class Field extends Shape{
 
 	private void drawCircle(GL2 gl, double radius, double centerX, double centerY, double r, double g, double b, double alpha){
 		// Color portion of the circle
+		gl.glLineWidth(10);
 		gl.glBegin(GL2.GL_TRIANGLE_FAN);
 		setColor(gl, r, g, b, alpha);
 		gl.glVertex2d(centerX,centerY);
@@ -109,7 +107,7 @@ public class Field extends Shape{
 
 		// White it out
 		gl.glBegin(GL2.GL_TRIANGLE_FAN);
-		setColor(gl, 255, 255, 255);
+		setColor(gl, 255,255,255);
 		gl.glVertex2d(centerX, centerY);
 		for (int i=0; i<=32; i++)
 		{
@@ -134,17 +132,84 @@ public class Field extends Shape{
 				gl.glVertex2d(x,y);
 			}
 			gl.glEnd();
+			drawLinesInCircles(gl, radius*.1, centerX, centerY);
 		}
-
+		gl.glLineWidth(1);
 
 	}
 
-	private void drawLinesInCircles(GL2 gl){
+	private void drawLinesInCircles(GL2 gl, double radius, double centerX, double centerY){
+		gl.glBegin(GL2.GL_LINES);
+		gl.glLineWidth(.5f);
+			// Top right
+			gl.glVertex2d(centerX + convertWidth(radius), centerY + convertHeight(radius));
+			gl.glVertex2d(centerX + convertWidth(radius), centerY + 3*convertHeight(radius));
+			gl.glVertex2d(centerX + convertWidth(radius), centerY + convertHeight(radius));
+			gl.glVertex2d(centerX + 3*convertWidth(radius), centerY + convertHeight(radius));
 
+			// Top left
+			gl.glVertex2d(centerX - convertWidth(radius), centerY + convertHeight(radius));
+			gl.glVertex2d(centerX - convertWidth(radius), centerY + 3*convertHeight(radius));
+			gl.glVertex2d(centerX - convertWidth(radius), centerY + convertHeight(radius));
+			gl.glVertex2d(centerX - 3*convertWidth(radius), centerY + convertHeight(radius));
+
+			// Bottom right
+			gl.glVertex2d(centerX + convertWidth(radius), centerY - convertHeight(radius));
+			gl.glVertex2d(centerX + convertWidth(radius), centerY - 3*convertHeight(radius));
+			gl.glVertex2d(centerX + convertWidth(radius), centerY - convertHeight(radius));
+			gl.glVertex2d(centerX + 3*convertWidth(radius), centerY - convertHeight(radius));
+
+			// Bottom left
+			gl.glVertex2d(centerX - convertWidth(radius), centerY - convertHeight(radius));
+			gl.glVertex2d(centerX - convertWidth(radius), centerY - 3*convertHeight(radius));
+			gl.glVertex2d(centerX - convertWidth(radius), centerY - convertHeight(radius));
+			gl.glVertex2d(centerX - 3*convertWidth(radius), centerY - convertHeight(radius));
+		gl.glEnd();
+		gl.glLineWidth(1);
 	}
 
 	private void loadImageCenterIce(GL2 gl){
+		// TODO I cheated and used 2d graphics to add this image.. turns out adding a png is more difficult
+		//Image logo = Utilities.fullyLoadImage("wild_logo.png");
+	}
 
+
+
+	private void drawGoalBoundary(GL2 gl){
+		double x = convertWidth(view.getWidth()*percentWidth);
+		double y = convertHeight(view.getHeight()*percentHeight*(1.0/3));
+
+		// Under portion of goal boundaries
+		setColor(gl, 255, 0, 0, 255);
+		gl.glBegin(GL2.GL_POLYGON);
+			gl.glVertex2d(x, y);
+			gl.glVertex2d(x - x*(1.0/9), y);
+			gl.glVertex2d(x - x*(1.0/9), -y);
+			gl.glVertex2d(x, -y);
+		gl.glEnd();
+
+		gl.glBegin(GL2.GL_POLYGON);
+			gl.glVertex2d(-x, y);
+			gl.glVertex2d(-x + x*(1.0/9), y);
+			gl.glVertex2d(-x + x*(1.0/9), -y);
+			gl.glVertex2d(-x, -y);
+		gl.glEnd();
+
+		// Above portion of goal boundaries
+		setColor(gl, 68, 214, 243, 200);
+		gl.glBegin(GL2.GL_POLYGON);
+			gl.glVertex2d(x, y*.95);
+			gl.glVertex2d((x - x*(1.0/9)*.95), y*.95);
+			gl.glVertex2d((x - x*(1.0/9)*.95), -y*.95);
+			gl.glVertex2d(x, -y*.95);
+		gl.glEnd();
+
+		gl.glBegin(GL2.GL_POLYGON);
+			gl.glVertex2d(-x, y*.95);
+			gl.glVertex2d((-x + x*(1.0/9)*.95), y*.95);
+			gl.glVertex2d((-x + x*(1.0/9)*.95), -y*.95);
+			gl.glVertex2d(-x, -y*.95);
+		gl.glEnd();
 	}
 
 }
